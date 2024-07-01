3d25b7a43c5e70750b986dba0f2c26dc6b041a0c
Euxen <kelvinhezhang@live.com>
2024-07-01 17:12:08 -0400

    added echo response

--- app/main.py
+++ app/main.py
@@ -7,12 +7,19 @@ def main():
 
     client_socket, client_address = server_socket.accept()
     
-    response = "HTTP/1.1 200 OK\r\n\r\n"
+    response = ""
 
     data = client_socket.recv(1024).decode("utf-8").split("\r\n")
 
-    if data[0].split(" ")[1] != "/":
+    if data[0].split(" ")[1] == "/" or data[0].split(" ")[1] == "/":
 
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    elif data[0].split(" ")[1].startswith("/echo/"):
+
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(url[6:])}\r\n\r\n{url[6:]}".encode()
+
+    else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
     client_socket.sendall(response.encode("utf-8"))
