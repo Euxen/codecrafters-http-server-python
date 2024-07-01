b39fe2b2f506acfa9d45df586fe4a6df96fe414f
Euxen <kelvinhezhang@live.com>
2024-07-01 16:59:01 -0400

    extracting url and giving a response based on extracted url

--- app/main.py
+++ app/main.py
@@ -4,7 +4,19 @@ import socket
 def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # respond with a 200 OK response
+
+    client_socket, client_address = server_socket.accept()
+    
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    data = client_socket.recv(1024).decode("utf-8").split("\r\n")
+
+    if data[0].split(" ")[1] != "/":
+
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    client_socket.sendall(response.encode("utf-8"))
+
 
 
 if __name__ == "__main__":
